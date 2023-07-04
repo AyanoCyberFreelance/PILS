240dcc8b9bda151223c5444c3b98465f1082397a
Dmitry Khamidullin <ayanocyber.freelance.gmail@com>
2023-07-04 09:18:13 +0500

    sixth 6 commit

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,18 @@
 function delNode(adress){adress.parentNode.parentNode.remove()}
 
 function delNode(prod){prod.parentNode.parentNode.remove()}
+
+document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("rmv").remove()
+})
+
+$(document).ready(function() {
+$("#show_bar").click(function() {
+$("#bar_block").slideToggle();
+if ($("#show_bar").html() == 'Показать штрихкод') {
+$("#show_bar").html('Скрыть штрихкод');
+} else {
+$("#show_bar").html('Показать штрихкод');
+}
+});
+});
