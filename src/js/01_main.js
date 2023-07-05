c5e39ae359782ce5ef32ad8c7a35e5f80bb5f2b8
Dmitry Khamidullin <ayanocyber.freelance.gmail@com>
2023-07-05 17:51:15 +0500

    eight 8 commit

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,5 @@
+// Удаление некоторых пунктов корзины товаров или другой страницы
+
 function delNode(adress){adress.parentNode.parentNode.remove()}
 
 function delNode(prod){prod.parentNode.parentNode.remove()}
@@ -5,6 +7,8 @@ function delNode(prod){prod.parentNode.parentNode.remove()}
 document.getElementById("btn").addEventListener("click", () => {
     document.getElementById("rmv").remove();
 })
+
+// Отображение и исчезновение категорий на странице товара
 
  function viewdiv(id) {
         var el = document.getElementById(id);
@@ -25,4 +29,42 @@ document.getElementById("btn").addEventListener("click", () => {
             el.style.display = "block";
         }
     }
+
+// Избавление ссылок от функции перехода по ним
+document.querySelectorAll("a[href='#']").forEach(el => { el.addEventListener("click", () => 0); })
+
+
+// Счётчик числа добавленных товаров
+var splashInput = document.querySelector(".cart_left_num");
+var splashVal = parseFloat(splashInput.value);
+document.querySelector(".cart_left_plus").onclick = function() {
+  splashInput.value = ++splashVal;
+}
+document.querySelector(".cart_left_minus").onclick = function() {
+  if (splashVal > 1) {
+    splashInput.value = --splashVal;
+  } 
+} 
+
+var splashInput = document.querySelector(".cart_left_num--3");
+var splashVal = parseFloat(splashInput.value);
+document.querySelector(".cart_left_plus--3").onclick = function() {
+  splashInput.value = ++splashVal;
+}
+document.querySelector(".cart_left_minus--3").onclick = function() {
+  if (splashVal > 1) {
+    splashInput.value = --splashVal;
+  } 
+}
+
+var splashInput = document.querySelector(".cart_left_num--2");
+var splashVal = parseFloat(splashInput.value);
+document.querySelector(".cart_left_plus--2").onclick = function() {
+  splashInput.value = ++splashVal;
+}
+document.querySelector(".cart_left_minus--2").onclick = function() {
+  if (splashVal > 1) {
+    splashInput.value = --splashVal;
+  } 
+}
     
