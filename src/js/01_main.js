53a3158048bbe6b080370ff539e37fb6692e9b95
Dmitry Khamidullin <ayanocyber.freelance.gmail@com>
2023-07-06 13:49:23 +0500

    ninth 9 commit

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,15 +1,25 @@
 // Удаление некоторых пунктов корзины товаров или другой страницы
-
 function delNode(adress){adress.parentNode.parentNode.remove()}
 
 function delNode(prod){prod.parentNode.parentNode.remove()}
 
-document.getElementById("btn").addEventListener("click", () => {
-    document.getElementById("rmv").remove();
-})
+// Замена некоторых пунктов корзины товаров или другой страницы
+function switchVisible() {
+            if (document.getElementById('rmv')) {
 
-// Отображение и исчезновение категорий на странице товара
+                if (document.getElementById('rmv').style.display == 'block') {
+                    document.getElementById('act').style.display = 'none';
+                    document.getElementById('btnS').style.display = 'block';
+                }
+                else {
+                    document.getElementById('act').style.display = 'block';
+                    document.getElementById('rmv').style.display = 'none';
+                    document.getElementById('btnS').style.display = 'none';
+                }
+            }
+}
 
+// Отображение и исчезновение пунктов описание\отзыв на странице товара
  function viewdiv(id) {
         var el = document.getElementById(id);
         var link = document.getElementById('btn1');
@@ -30,41 +40,75 @@ document.getElementById("btn").addEventListener("click", () => {
         }
     }
 
+
 // Избавление ссылок от функции перехода по ним
 document.querySelectorAll("a[href='#']").forEach(el => { el.addEventListener("click", () => 0); })
 
 
 // Счётчик числа добавленных товаров
-var splashInput = document.querySelector(".cart_left_num");
-var splashVal = parseFloat(splashInput.value);
-document.querySelector(".cart_left_plus").onclick = function() {
-  splashInput.value = ++splashVal;
-}
-document.querySelector(".cart_left_minus").onclick = function() {
-  if (splashVal > 1) {
-    splashInput.value = --splashVal;
-  } 
-} 
+$('.cart_left_count').click(function(e) {
+  const change = +e.target.dataset.change;
+  if (change) {
+    $('.cart_left_number', this).text((i, text) => `${parseInt(text, 10) + change} шт`);
+  }
+});
 
-var splashInput = document.querySelector(".cart_left_num--3");
-var splashVal = parseFloat(splashInput.value);
-document.querySelector(".cart_left_plus--3").onclick = function() {
-  splashInput.value = ++splashVal;
-}
-document.querySelector(".cart_left_minus--3").onclick = function() {
-  if (splashVal > 1) {
-    splashInput.value = --splashVal;
-  } 
+// Аккорден для страницы с товаром чтобы описание\отзыв выпадали при нажатии на кнопку
+function findElements(object, element) {
+	const instance = object;
+	instance.element = element;
+	instance.target = element.nextElementSibling;
 }
 
-var splashInput = document.querySelector(".cart_left_num--2");
-var splashVal = parseFloat(splashInput.value);
-document.querySelector(".cart_left_plus--2").onclick = function() {
-  splashInput.value = ++splashVal;
+function hideElement(object) {
+	const instance = object;
+	const { target } = instance;
+	target.style.height = null;
+	instance.isActive = false;
 }
-document.querySelector(".cart_left_minus--2").onclick = function() {
-  if (splashVal > 1) {
-    splashInput.value = --splashVal;
-  } 
+
+function showElement(object) {
+	const instance = object;
+	const { target, height } = instance;
+	target.style.height = `${height}px`;
+	instance.isActive = true;
 }
+
+function changeElementStatus(instance) {
+	if (instance.isActive) {
+		hideElement(instance);
+	} else {
+		showElement(instance);
+	}
+}
+
+function measureHeight(object) {
+	const instance = object;
+	instance.height = object.target.firstElementChild.clientHeight;
+}
+
+function subscribe(instance) {
+	instance.element.addEventListener('click', (event) => {
+		event.preventDefault();
+		changeElementStatus(instance);
+	});
+	window.addEventListener('resize', () => measureHeight(instance));
+}
+
+function accordion(element) {
+	const instance = {};
+		
+	function init() {
+		findElements(instance, element);
+		measureHeight(instance);
+		subscribe(instance);
+	}
+	
+	init();
+}
+
+const elements = [...document.querySelectorAll('.js-accordion')];
+elements.forEach(accordion);
+
+
     
