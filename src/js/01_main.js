8d9a3076538923401246be9993396c64a28e3b21
Dmitry Khamidullin <ayanocyber.freelance.gmail@com>
2023-07-07 16:50:33 +0500

    eleventh 11 commit

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -193,5 +193,67 @@ $('#zoom_04').ezPlus({
     lensSize: 150
 });
 
+// Кнопка для скролла на начало страницы при нажатии
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  scrolling: false,
+  show() {
+    if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.remove('btn-up_hide');
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  hide() {
+    if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.add('btn-up_hide');
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  addEventListener() {
+    // при прокрутке окна (window)
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (this.scrolling && scrollY > 0) {
+        return;
+      }
+      this.scrolling = false;
+      // если пользователь прокрутил страницу более чем на 200px
+      if (scrollY > 400) {
+        // сделаем кнопку .btn-up видимой
+        this.show();
+      } else {
+        // иначе скроем кнопку .btn-up
+        this.hide();
+      }
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      this.scrolling = true;
+      this.hide();
+      // переместиться в верхнюю часть страницы
+      window.scrollTo({
+        top: 0, behavior: 'smooth'
+      });
+    }
+  }
+}
 
-    
+btnUp.addEventListener();
+
+// Затемнение общего фона при нажатии на input
+$('#form_bg').on('click', function() {
+  $(".d2").toggleClass("shadow");
+});
+$('#main_bg').on('click', function() {
+  $(this).removeClass("shadow");
+});
+
+$('#main_bg').on('click', function() {
+  $(".d2").removeClass("shadow");
+});
